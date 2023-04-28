23a293cc6ab6668aa2c4418b06d2810094ba712d
Jinyoung Rhee <jakerhee321@gmail.com>
2023-04-28 18:39:32 +0900

    Print Board Function

--- game.py
+++ game.py
@@ -3,3 +3,7 @@ class TicTacToe:
         self.board = [' ' for _ in range(9)]
         self.current_winner = None
         
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('|' + '|'. join(row) + '|')
+        
