35c692e35dbdab4226ae1b1167ce991e2034658f
Jinyoung Rhee <jakerhee321@gmail.com>
2023-04-30 19:18:12 +0900

    Available_moves Function

--- game.py
+++ game.py
@@ -13,4 +13,10 @@ class TicTacToe:
         for row in number_board:
             print('|' + '|'. join(row) + '|')
 
-    
+    def available_moves(self):
+        moves = []
+        for (i,spot) in enumerate(self.board):
+            if spot == ' ':
+                moves.append(i)
+        return moves
+        # return [i for i, spot in enumerate(self.board) if spot == ' ']
