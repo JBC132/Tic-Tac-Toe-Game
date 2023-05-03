1509466d682382d1cd7d55815dd3422b3334bedd
Jinyoung Rhee <jakerhee321@gmail.com>
2023-05-04 08:51:17 +0900

    More Functions

--- game.py
+++ game.py
@@ -21,10 +21,19 @@ class TicTacToe:
         return moves
         # return [i for i, spot in enumerate(self.board) if spot == ' ']
 
+    def empty_squares(self):
+        return ' ' in self.board
+    
+    def num_empty_squares(self):
+        return self.board.count(' ')
+    
 def play(game, x_player, o_player, print_game = True):
     if print_game:
         game.print_board_nums()
 
     letter = 'X'
     while game.empty_squares():
-        pass
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
