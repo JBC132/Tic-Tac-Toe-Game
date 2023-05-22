b6bf2c1fdcad6aab5391ee14e3d1045f202267c9
Jinyoung Rhee <jakerhee321@gmail.com>
2023-05-22 13:29:10 +0900

    Minimax base cases

--- TicTacToe.py
+++ TicTacToe.py
@@ -45,3 +45,16 @@ class GeniusComputerPlayer(Player):
         else:
             square = self.minmax(game, self.letter)
         return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None,
+                    'score': 1 * (state.num_empty_square() + 1) if other_player == max_player 
+                    else -1 * (state.num_empty_square() + 1)
+                    }
+        
+        elif not state.empty_squares():
+            return {'position': None, 'score':0}
